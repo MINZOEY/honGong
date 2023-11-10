17a9e5682d65d14229035d9efe7a3598cfc0643e
MINZOEY <mj8435@naver.com>
2023-11-10 20:12:01 +0900

    고차함수

--- collectEggs/app.js
+++ collectEggs/app.js
@@ -72,3 +72,21 @@ const add = function (x, y){
 }
 add(3,4)
 // 7
+
+// 218. 고차 함수
+function callTwice(func){
+	func();
+	func();
+};
+function callTenTimes(f){
+	for(let i = 0; i <10; i++){
+		f()
+	}
+};
+function rollDie(){
+	const roll = Math.floor(Math.random() * 6) + 1
+	console.log(roll)
+};
+
+callTwice(rollDie);
+callTenTimes(rollDie);
